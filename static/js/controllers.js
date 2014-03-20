a7999c8b57d471a84286c32983cd1d1050c2aa7f
Salil Panikkaveettil <salilpa007@gmail.com>
2014-03-20 18:07:36 +0530

    using video params instead of phone

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,16 +1,20 @@
 var youtubeApp = angular.module('youtubeApp', []);
 
-youtubeApp.config(function($interpolateProvider) {
+youtubeApp.config(function ($interpolateProvider) {
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
 });
 youtubeApp.controller('VideoListCtrl', function ($scope) {
-    $scope.phones = [
-        {'name': 'Nexus S',
-            'snippet': 'Fast just got faster with Nexus S.'},
-        {'name': 'Motorola XOOM™ with Wi-Fi',
-            'snippet': 'The Next, Next Generation tablet.'},
-        {'name': 'MOTOROLA XOOM™',
-            'snippet': 'The Next, Next Generation tablet.'}
+    $scope.videos = [
+        {'title':'Queen - Bohemian Rhapsody (Official Video)',
+            'description':"Subscribe to the Official Queen Channel Here http://bit.ly/Subscribe2Queen Queen - 'Bohemian Rhapsody' The official 'Bohemian Rhapsody' music video.",
+            'image': 'https://i.ytimg.com/vi/fJ9rUzIMcZQ/hqdefault.jpg',
+            'id': 'fJ9rUzIMcZQ'
+        },
+        {'title':'Metallica - Nothing Else Matters [Official Music Video]',
+            'description':'SONG FACTS: Singer and rhythm guitarist \"James Hetfield\" wrote this song while he was on the phone with his girlfriend at this time. Since he held the phone ...',
+            'image': 'https://i.ytimg.com/vi/Tj75Arhq5ho/hqdefault.jpg',
+            'id': 'Tj75Arhq5ho'
+        }
     ];
 });
